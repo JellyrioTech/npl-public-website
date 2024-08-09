7de6b762f2adc4d1cad0dd7df77e1f494019ac08
Shubroto Shuvo <51680632+shuvofifty@users.noreply.github.com>
2024-08-09 09:58:38 -0400

    Download Page update completed

--- src/screens/DownloadPage.tsx
+++ src/screens/DownloadPage.tsx
@@ -3,6 +3,7 @@ import backgroundImage from "../../public/backgroundArt.jpg";
 import darkLogo from "../../public/App_logo_dark.png";
 import appstoreDownload from "../assets/App Store download.png";
 import playstoreDownload from "../assets/Google Play download.png";
+import amazonAppStore from "../assets/amazon_appstore.png";
 
 function DownloadPage() {
     return (
@@ -11,7 +12,7 @@ function DownloadPage() {
             style={{ backgroundImage: `url(${backgroundImage})` }}
         >
             <div className="flex items-center justify-center px-4 py-6 md:ml-4">
-                <div className="bg-white p-8 rounded-xl max-w-[800px] w-full pb-[200px] shadow-[inset_0px_1px_15px_1px_#22543d] md:pb-0">
+                <div className="bg-white p-8 rounded-xl max-w-[800px] w-full pb-[700px] shadow-[inset_0px_1px_15px_1px_#22543d] md:pb-0">
                     <div className="flex space-x-4 items-center mb-4">
                         <div className="relative w-16 h-16 drop-shadow-md">
                             <img
@@ -34,6 +35,14 @@ function DownloadPage() {
                                     </span>{" "}
                                     the app
                                 </p>
+                                <p className="font-semibold">
+                                    <span className="">
+                                        For android, please download from amazon
+                                        app store. The instruction are give in a
+                                        different section
+                                    </span>{" "}
+                                    the app
+                                </p>
                                 <div className="flex space-x-1">
                                     <a
                                         href="https://apps.apple.com/us/app/nesterin-pickleball-league/id6590604559"
@@ -44,12 +53,21 @@ function DownloadPage() {
                                             className="w-[120px]"
                                         />
                                     </a>
-                                    <a href="www.google.com" target="_blank">
+                                    <a
+                                        href="https://www.amazon.com/gp/product/B0DCKB7ZM6"
+                                        target="_blank"
+                                    >
+                                        <img
+                                            src={amazonAppStore}
+                                            className="w-[120px]"
+                                        />
+                                    </a>
+                                    {/* <a href="www.google.com" target="_blank">
                                         <img
                                             src={playstoreDownload}
                                             className="w-[120px]"
                                         />
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
 
@@ -84,6 +102,39 @@ function DownloadPage() {
                                 <li>
                                     Go through the tournament details and rules.
                                     Finally Register for the tournament
+                                </li>
+                            </ol>
+                            <p>
+                                <b>For Android to download the app</b>
+                            </p>
+                            <ol className="list-decimal list-inside space-y-2 mt-5 mb-4">
+                                <li>
+                                    Visit{" "}
+                                    <a
+                                        href="https://www.amazon.com/gp/mas/get/amazonapp"
+                                        target="_blank"
+                                        className="text-blue-700 font-bold"
+                                    >
+                                        Amazon app store download
+                                    </a>{" "}
+                                    page
+                                </li>
+                                <li>Tap get amazon appstore button</li>
+                                <li>
+                                    Once downloaded tap on the image below here
+                                </li>
+                                <a
+                                    href="https://www.amazon.com/gp/product/B0DCKB7ZM6"
+                                    target="_blank"
+                                >
+                                    <img
+                                        src={amazonAppStore}
+                                        className="w-[120px]"
+                                    />
+                                </a>
+                                <li>
+                                    Download the app, and follow the process as
+                                    above
                                 </li>
                             </ol>
                         </div>
